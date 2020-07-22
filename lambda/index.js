e9df9e9ef8e112634413c7ba3a7213f76c76b7b5
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2020-07-22 18:34:35 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -1,120 +1,289 @@
-// This sample demonstrates handling intents from an Alexa skill using the Alexa Skills Kit SDK (v2).
-// Please visit https://alexa.design/cookbook for additional examples on implementing slots, dialog management,
-// session persistence, api calls, and more.
-const Alexa = require('ask-sdk-core');
-
-const LaunchRequestHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'LaunchRequest';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Welcome, you can say Hello or Help. Which would you like to try?';
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
+{
+    "interactionModel": {
+        "languageModel": {
+            "invocationName": "radio",
+            "intents": [
+                {
+                    "name": "AMAZON.CancelIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.HelpIntent",
+                    "samples": [
+                        "help"
+                    ]
+                },
+                {
+                    "name": "AMAZON.StopIntent",
+                    "samples": [
+                        "stop"
+                    ]
+                },
+                {
+                    "name": "AMAZON.RepeatIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.YesIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.NoIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.FallbackIntent",
+                    "samples": [
+                        "test"
+                    ]
+                },
+                {
+                    "name": "AMAZON.ResumeIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.PauseIntent",
+                    "samples": []
+                },
+                {
+                    "name": "PlayRadioIntent",
+                    "slots": [
+                        {
+                            "name": "station",
+                            "type": "SEARCH_TERM"
+                        }
+                    ],
+                    "samples": [
+                        "{station}",
+                        "for {station}",
+                        "play {station} music",
+                        "play some {station}",
+                        "play {station}",
+                        "{station} music"
+                    ]
+                },
+                {
+                    "name": "AMAZON.NextIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.NavigateHomeIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.ShuffleOffIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.ShuffleOnIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.MoreIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.NavigateSettingsIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.PageUpIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.PageDownIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.PreviousIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.ScrollRightIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.ScrollDownIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.ScrollLeftIntent",
+                    "samples": []
+                },
+                {
+                    "name": "AMAZON.ScrollUpIntent",
+                    "samples": []
+                },
+                {
+                    "name": "ControlRadioIntent",
+                    "slots": [
+                        {
+                            "name": "control",
+                            "type": "CONTROL_TYPE"
+                        },
+                        {
+                            "name": "duration",
+                            "type": "AMAZON.DURATION"
+                        }
+                    ],
+                    "samples": [
+                        "{control} ",
+                        "{control} {duration}"
+                    ]
+                }
+            ],
+            "types": [
+                {
+                    "name": "SEARCH_TERM",
+                    "values": [
+                        {
+                            "name": {
+                                "value": "fountain"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "suspended fed strongly room"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "great area briefly force"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "frequent treat little ban"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "size finishing"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "previous black"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "optional march"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "revealed emphasis"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "away chain"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "standing twelve"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "head independent"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "commercial defining"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "upset horse"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "entitled stay"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "typed ease"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "taking comment advice"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "implemented small hour"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "excess background favor"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "leading extract square"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "removed white summary"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "state old post"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "diverse international parity"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "thorough reserve forecast"
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "bla bla bla"
+                            }
+                        }
+                    ]
+                },
+                {
+                    "name": "CONTROL_TYPE",
+                    "values": [
+                        {
+                            "name": {
+                                "value": "rewind",
+                                "synonyms": [
+                                    "skip back",
+                                    "go back",
+                                    "back"
+                                ]
+                            }
+                        },
+                        {
+                            "name": {
+                                "value": "forward",
+                                "synonyms": [
+                                    "go forward",
+                                    "skip ahead",
+                                    "fast forward"
+                                ]
+                            }
+                        }
+                    ]
+                }
+            ]
+        }
     }
-};
-const HelloWorldIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'HelloWorldIntent';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Hello World!';
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
-    }
-};
-const HelpIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
-    },
-    handle(handlerInput) {
-        const speakOutput = 'You can say hello to me! How can I help?';
-
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
-    }
-};
-const CancelAndStopIntentHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest'
-            && (Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.CancelIntent'
-                || Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.StopIntent');
-    },
-    handle(handlerInput) {
-        const speakOutput = 'Goodbye!';
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .getResponse();
-    }
-};
-const SessionEndedRequestHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'SessionEndedRequest';
-    },
-    handle(handlerInput) {
-        // Any cleanup logic goes here.
-        return handlerInput.responseBuilder.getResponse();
-    }
-};
-
-// The intent reflector is used for interaction model testing and debugging.
-// It will simply repeat the intent the user said. You can create custom handlers
-// for your intents by defining them above, then also adding them to the request
-// handler chain below.
-const IntentReflectorHandler = {
-    canHandle(handlerInput) {
-        return Alexa.getRequestType(handlerInput.requestEnvelope) === 'IntentRequest';
-    },
-    handle(handlerInput) {
-        const intentName = Alexa.getIntentName(handlerInput.requestEnvelope);
-        const speakOutput = `You just triggered ${intentName}`;
-
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            //.reprompt('add a reprompt if you want to keep the session open for the user to respond')
-            .getResponse();
-    }
-};
-
-// Generic error handling to capture any syntax or routing errors. If you receive an error
-// stating the request handler chain is not found, you have not implemented a handler for
-// the intent being invoked or included it in the skill builder below.
-const ErrorHandler = {
-    canHandle() {
-        return true;
-    },
-    handle(handlerInput, error) {
-        console.log(`~~~~ Error handled: ${error.stack}`);
-        const speakOutput = `Sorry, I had trouble doing what you asked. Please try again.`;
-
-        return handlerInput.responseBuilder
-            .speak(speakOutput)
-            .reprompt(speakOutput)
-            .getResponse();
-    }
-};
-
-// The SkillBuilder acts as the entry point for your skill, routing all request and response
-// payloads to the handlers above. Make sure any new handlers or interceptors you've
-// defined are included below. The order matters - they're processed top to bottom.
-exports.handler = Alexa.SkillBuilders.custom()
-    .addRequestHandlers(
-        LaunchRequestHandler,
-        HelloWorldIntentHandler,
-        HelpIntentHandler,
-        CancelAndStopIntentHandler,
-        SessionEndedRequestHandler,
-        IntentReflectorHandler, // make sure IntentReflectorHandler is last so it doesn't override your custom intent handlers
-    )
-    .addErrorHandlers(
-        ErrorHandler,
-    )
-    .lambda();
+}
