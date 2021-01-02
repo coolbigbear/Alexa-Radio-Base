51be0b2f3ee6bf398f4facc1be09976774d03423
AlexaHostedSkills <VoiceHubCodeCommitRole/provisioning>
2021-01-02 16:03:50 +0000

    Initial Commit

---
+++ lambda/AudioController.js
@@ -0,0 +1,73 @@
+class AlexaResponses {
+
+    constructor() {
+    
+    }
+
+    speak(speech) {
+        this.outputSpeech = {
+            "type": "SSML",
+            "ssml": `<speak>${speech}</speak>`
+        };
+
+        return this;
+    }
+
+    prompt(speech) {
+        this.response.reprompt = {
+            outputSpeech: {
+                "type": "SSML",
+                "ssml": `<speak>${speech}</speak>`
+            }
+        }
+
+        return this;
+    }
+
+    card(title, content = '', image_url = '') {
+        this.response.card = {
+            "type": "Standard",
+            "title": `${title}`,
+            "content": `${content}`,
+            "text": `${content}`,
+            "image": {
+                "smallImageUrl": `${image_url}`,
+                "largeImageUrl": `${image_url}`
+            }
+        }
+        return this;
+    }
+
+    play(station) {
+
+        this.directives = [
+            {
+                "type": "AudioPlayer.Play",
+                "playBehavior": "REPLACE_ALL",
+                "audioItem": {
+                    "stream": {
+                        "url": `${station.url}`,
+                        "token": `${station.token}`,
+                        "offsetInMilliseconds": `${station.progress}`
+                    }
+                }
+            }
+        ]
+
+        return this;
+    }
+
+    stop() {
+        this.directives = [
+            {
+                "type": "AudioPlayer.Stop"
+            }
+        ]
+
+        this.shouldEndSession = true;
+
+        return this;
+    }
+
+}
+module.exports = new AlexaResponses();
