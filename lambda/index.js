b2d337a55817d9e09abaf123625684d9044882f2
Michal Czuper <mrczuper@gmail.com>
2021-01-11 16:11:40 +0000

    Bug fix

--- lambda/index.js
+++ lambda/index.js
@@ -92,7 +92,7 @@ const GetSongIntentHandler = {
 					SONG.artist = SONG.artist.replace(" / ", " , and ")
 				} else {
 					let array = []
-					for (let i = 0; i < split.length; i++) {
+					for (let i = 0; i < split.length - 1; i++) {
 						array.push(split[i])
 						if (i == split.length - 2) {
 							array.push(", and ")
