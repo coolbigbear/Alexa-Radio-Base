535bd4f2e77b7b906e0b0304a3f2c806205cda12
Michal Czuper <mrczuper@gmail.com>
2021-01-15 16:43:39 +0000

    Added dots for Alexa when says rmf fm

--- lambda/index.js
+++ lambda/index.js
@@ -11,9 +11,9 @@ const Alexa = require("ask-sdk-core")
 
 const SONG_URL = "https://www.rmfon.pl/stacje/ajax_playing_main.txt"
 const STATION_URL = "http://rmfon.pl/stacje/flash_aac_5.xml.txt"
-const STATION_NAME = "RMF FM"
+const STATION_NAME = "R. M. F. F. M."
 const STATION_CHANNEL = "Poland"
-const HERE_IS = "Here is"
+const HERE_IS = "Here is,"
 
 let STATION = {
 	name: STATION_NAME,
@@ -30,7 +30,7 @@ let SONG = {
 	year: ""
 }
 
-const NEW_STREAM_MESSAGE = `${HERE_IS} - ${STATION.name}, from ${STATION.channel}.`
+const NEW_STREAM_MESSAGE = `${HERE_IS} ${STATION.name}`
 const RESUMING_MESSAGE = `Resuming ${STATION_NAME}`
 const STOP_MESSAGE = "Stopping!"
 
@@ -87,7 +87,7 @@ const HelpIntentHandler = {
 	},
 	handle(handlerInput) {
 
-		const speakOutput = `To listen to, radio ${STATION_NAME} simply say, open radio ${STATION_NAME}`
+		const speakOutput = `To listen to, radio ${STATION_NAME}, simply say, open radio ${STATION_NAME}`
 		console.log(`Help intent handler triggered: ${JSON.stringify(handlerInput)}`)
 
 		return handlerInput.responseBuilder
