e20791cb2b09b025950bd30bb53da7404a91b880
Michal Czuper <mrczuper@gmail.com>
2021-02-09 13:20:39 +0000

    Changes

--- lambda/DynamoDB.js
+++ lambda/DynamoDB.js
@@ -70,7 +70,7 @@ async function getStationInfo() {
 	console.log(`DEV --- ${unpacked}`)
 	console.log("Unpacked", unpacked)
 	console.log("Unpacked %s", unpacked)
-	console.log(JSON.stringify(unpacked))
+	console.log(JSON.stringify(AWS.DynamoDB.Converter.unmarshall(tableData)))
 	console.log(console.dir(unpacked, { depth: null }))
 	const util = require('util')
 
